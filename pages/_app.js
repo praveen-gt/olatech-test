6c42143032ec63abd38739d1dd525f3144890620
Praveen Tewatia <praveen@Praveens-MacBook-Pro.local>
2022-10-26 15:36:27 +0530

    tailwind add

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import { useEffect } from "react";
+
+import "../styles/globals.css"
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    useEffect(() => {
+        import('tw-elements');
+    }, []);
+    return (
+            <Component {...pageProps} />
+
+    )
 }
 
 export default MyApp
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "class",
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+      './pages/**/*.{js,ts,jsx,tsx}',
+      './components/**/*.{js,ts,jsx,tsx}',
+      './node_modules/tw-elements/dist/js/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+      extend: {},
   },
-  plugins: [],
-}
+  plugins: [
+      require('tw-elements/dist/plugin')
+  ], }
