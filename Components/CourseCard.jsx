fe443ecb9a9bfd46a7aaae04fc44ce57748cde76
Praveen Tewatia <praveen@Praveens-MacBook-Pro.local>
2022-10-26 17:42:15 +0530

    catalog page updated

--- Components/CourseCard.jsx
+++ Components/CourseCard.jsx
@@ -5,6 +5,8 @@ import { AiFillFormatPainter } from 'react-icons/ai'
 import { GiClayBrick, GiDominoTiles, GiStoneWall } from 'react-icons/gi'
 import { MdCarpenter } from 'react-icons/md'
 import { HiBuildingOffice } from 'react-icons/hi2'
+import Lottie from 'react-lottie';
+import data from '../public/static/course-data.json'
 
 const CourseCard = ({
     icon,
@@ -16,16 +18,25 @@ const CourseCard = ({
 }) => {
     const [color, setColor] = useState('#d5d5d5')
 
+    const defaultOptions = {
+        loop: true,
+        autoplay: true,
+        animationData: data,
+        rendererSettings: {
+            preserveAspectRatio: 'xMidYMid slice'
+        }
+    };
+
     return (
 
-        <div className={Styles.boxContainer} 
-        onMouseOver={()=>{
-            setColor('#0009')
-             
-        }}
-        onMouseOut ={()=>{
-            setColor('#d5d5d5')
-        }}
+        <div className={Styles.boxContainer}
+            onMouseOver={() => {
+                setColor('#0009')
+
+            }}
+            onMouseOut={() => {
+                setColor('#d5d5d5')
+            }}
         >
             <Modal
                 title={title}
@@ -43,40 +54,54 @@ const CourseCard = ({
 
             >
                 <div className={Styles.container}>
-                <span className={Styles.features__text}>{num}</span>
-                <div className="features__svg" style={{
-                    marginBottom: 40
-                }}>
-                    {console.log(icon)}
+                    <span className={Styles.features__text}>{num}</span>
 
-                    {icon == "painting" &&
-                        <AiFillFormatPainter size={80} color = {color}/>
-                    }
-                    {icon == "brick" &&
-                        <GiClayBrick size={80} color = {color}/>
-                    }
-                    {icon == "carpentry" &&
-                        <MdCarpenter size={80} color = {color}/>
-                    }
-                    {icon == "tile" &&
-                        <GiStoneWall size={80} color = {color}/>
-                    }
-                    {icon == "building" &&
-                        <HiBuildingOffice size={80} color = {color}/>
-                    }
+                    <div className="features__svg" style={{
+                        marginBottom: 20
+                    }}>
 
-                </div>
-                <h4 style={{
-                    fontWeight: '600',
-                    fontSize: 19,
-                    width: '70%'
-                }} >{title}</h4>
-                <p style={{
-                    fontSize: 15
-                }}>Core Units: {core}</p>
-                <p style={{
-                    fontSize: 15
-                }}>Elective Units: {elective}</p>
+                        <Lottie options={defaultOptions}
+                            height={180}
+                            width={180}
+                            style={{
+                                marginLeft: 0
+                            }}
+                        // isStopped={this.state.isStopped}
+                        // isPaused={this.state.isPaused} 
+                        />
+                        {/* {console.log(icon)}
+
+                        {icon == "painting" &&
+                            <AiFillFormatPainter size={80} color={color} />
+                        }
+                        {icon == "brick" &&
+                            <GiClayBrick size={80} color={color} />
+                        }
+                        {icon == "carpentry" &&
+                            <MdCarpenter size={80} color={color} />
+                        }
+                        {icon == "tile" &&
+                            <GiStoneWall size={80} color={color} />
+                        }
+                        {icon == "building" &&
+                            <HiBuildingOffice size={80} color={color} />
+                        } */}
+
+                    </div>
+                    <h4 style={{
+                        fontWeight: '600',
+                        fontSize: 19,
+                        width: '90%',
+                        lineHeight: 1.4,
+                        marginBottom: 12
+                    }} >{title}</h4>
+                    <p style={{
+                        fontSize: 15
+                    }}>Core Units: {core}</p>
+                    <p style={{
+                        fontSize: 15
+                    }}>Elective Units: {elective}</p>
+                    {/* <p>Click here to see units</p> */}
                 </div>
             </Modal>
         </div>
