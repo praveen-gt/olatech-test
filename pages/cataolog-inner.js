5b725a8222a9667b2337118a3ddb4713b0dd4233
Praveen Tewatia <praveen@Praveens-MacBook-Pro.local>
2022-10-26 15:25:51 +0530

    changes done

--- pages/cataolog-inner.js
+++ pages/cataolog-inner.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Link from 'next/link'
 import Navbar from './navbar'
-import Banner from '../Components/banner'
+import Banner from '..Components/Banner'
 
 export default function catalog() {
   return (
